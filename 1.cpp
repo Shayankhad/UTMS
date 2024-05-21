cccff3ec041c2d5aed7410fc6eae361f50d83bcc
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-21 17:56:41 +0330

    a

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#define OK "OK"
+#define EMPTY "Empty"
+#define NOT_FOUND "Not Found"
+#define BAD_REQUEST "Bad Request"
+#define PERMISSION_DENIED "Permission Denied"
+using namespace std;
+
+int main (int argc , char* argv[]){
+    cout << argc;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+we have to extract those csv files
+1- majores 2- students 3- courses 4- prrofessors
+
+1) classes
+
+class majors
+
+class -> 
+1_major id / 2_ major name
+
+class students ->
+1- sid 2- name 3- major id 4- semester 5- pass
+
+courses -> 1- sid 2- name 3- credit 4- prerequisite 5-major_id
+
+proffesors -> 1- pid 2-nmae 3-major_id 4- position 5- pass
+
