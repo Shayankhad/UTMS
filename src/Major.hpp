20b713660300700a0d0861850197c56b2d23ae40
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-24 17:22:04 +0330

    added extract_majors_csv

--- src/Major.hpp
+++ src/Major.hpp
@@ -4,7 +4,14 @@
 
 class Major {
 public:
-
+    void major_csv_setup(int mid_ , string major_){
+        mid = mid_;
+        major = major_;
+    }
+    void test_show(){
+        cout << "mid: " << mid << endl;
+        cout << "major: "<< major << endl;
+    }
 private:
     int mid;
     string major;
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include "global.hpp"
 
-int main (int argc , char* argv[]){
-    for(int i = 0  ; i < argc ; i++ ){
-        cout << argv[i]<<endl;
+void extract_majors_csv(string address, vector<Major*> &majors){
+    fstream file(address);
+    if (!file.is_open()) {
+        cerr << "failed to open the majors file!!!";
     }
-    Student x;
+    string line;
+    getline (file , line);
+    while(getline(file , line)){
+        stringstream ss;
+        ss << line;
+        string id;
+        getline(ss , id , ',');
+        string major;
+        getline(ss , major);
+        Major* major_ptr = new Major;
+        major_ptr->major_csv_setup(string_to_int(id) , major);
+        majors.emplace_back(major_ptr);
+    }
+}
+
+int main (int argc , char* argv[]){
+    argc++;
+    argc--;
+    vector<Major*> majors;
+    extract_majors_csv(argv[1] , majors);
 }
