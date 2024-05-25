c6b20616ba261147d64d115a6442a35b36ff9078
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-25 23:31:51 +0330

    added run function for make the main shorter

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include "global.hpp"
 
-int main(int argc, char *argv[])
-{
-    argc++;
-    argc--;
-    vector<Major *> majors;
-    vector<Student *> students;
-    vector<Course *> courses;
-    vector<Professor *> professors;
-    UtAccount *ut_account_ptr = new UtAccount();
-    extract_majors_csv(argv[1], majors);
-    extract_students_csv(argv[2], students);
-    extract_courses_csv(argv[3], courses);
-    extract_professors_csv(argv[4], professors);
+void run(vector<Student *> &students , vector<Professor *> professors , UtAccount *ut_account_ptr){
     string command;
-
     while (true)
     {
         try{
@@ -36,6 +23,22 @@ int main(int argc, char *argv[])
         }
     }
 
+}
+
+int main(int argc, char *argv[])
+{
+    argc++;
+    argc--;
+    vector<Major *> majors;
+    vector<Student *> students;
+    vector<Course *> courses;
+    vector<Professor *> professors;
+    UtAccount *ut_account_ptr = new UtAccount();
+    extract_majors_csv(argv[1], majors);
+    extract_students_csv(argv[2], students);
+    extract_courses_csv(argv[3], courses);
+    extract_professors_csv(argv[4], professors);
+    run(students ,professors , ut_account_ptr);
     deallocate(majors ,students , courses ,professors , ut_account_ptr );
 
     return 0;
