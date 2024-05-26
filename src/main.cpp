cd9e75790f28741b69d90be794fbdd2107a92689
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-26 10:35:30 +0330

    added logout system

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,58 @@
 #include "global.hpp"
 
+// is_it_logout_command
+// logout_command
+// you have to use is_anyone_loged_in to check nobody logout from nothing
+// 
+bool is_it_logout_command(string command){
+    vector<string> commands = seperate_one_two_command(command);
+    stringstream ss;
+    ss << command;
+    string word;
+    int iteration = 0 ;
+    while (getline(ss, word, ' '))
+    {
+        iteration ++;
+    }
+    if(iteration == 3){
+        if((commands[0] == POST) && (commands[1] == LOGOUT)){
+            return true;
+        }else {
+            return false;
+        }
+    }else {
+        return false;
+    }
+}
+void logout_command(string command, vector<Student *> &students, vector<Professor *> &professors, UtAccount *ut_account_ptr){
+    vector<string> commands = seperate_one_two_command(command);
+    stringstream ss;
+    ss << command;
+    string word;
+    int iteration = 0 ;
+    while (getline(ss, word, ' '))
+    {
+        iteration ++;
+    }
+    if((iteration == 3) && (commands[0] == POST) && (commands[1] == LOGOUT)){
+        for(auto & student : students){
+            if(student->am_i_loged_in()){
+                student->set_loged(didnt_loged_in);
+            }
+        }
+        for(auto & professor : professors){
+            if(professor->am_i_loged_in()){
+                professor->set_loged(didnt_loged_in);
+            }
+        }
+        if(ut_account_ptr->am_i_loged_in()){
+            ut_account_ptr->set_loged(didnt_loged_in);
+        }
+        throw OkExeption();
+    }   
+}
+
+
 void run(vector<Student *> &students , vector<Professor *> &professors , UtAccount *ut_account_ptr){
     string command;
     while (true)
@@ -10,18 +63,29 @@ void run(vector<Student *> &students , vector<Professor *> &professors , UtAccou
             if(command == "qq"){
                 break;
             }
+
+            // first check
             check_for_four_commands(command);
             check_for_second_commands(command);
             check_for_quastion_mark(command);
+
+
+            // login command
             if(is_it_login_command(command)){
                 if(is_anyone_loged_in(students ,professors , ut_account_ptr)){
                     throw PermissionDenied();
                 }
             }
             login_command(command ,students ,professors , ut_account_ptr);
+            // logout command 
+            if(is_it_logout_command(command)){
+                if(!(is_anyone_loged_in(students ,professors , ut_account_ptr))){
+                    throw PermissionDenied();
+                }
+            }
+            logout_command(command ,students ,professors , ut_account_ptr);
 
-
-
+            // this should be at the end of the while loop: throw BadRequest();
             throw BadRequest();
         }
         catch(const MyException& e){
