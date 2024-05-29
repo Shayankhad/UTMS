f188e6032501d9f62cba07481de07646eeb034cc
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-29 16:39:05 +0330

    finished is_it_delete_post_command function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include "global.hpp"
 
 
+bool is_it_delete_post_command(string command){
+    // DELETE post ? id 3
+    vector<string> commands;
+    string word;
+    stringstream ss;
+    ss << command;
+    int iteration = 0;
+
+    while(getline(ss , word , ' ' )){
+        if(!word.empty()){
+            commands.push_back(word);
+            iteration ++;
+        }
+    }
+
+    if(iteration == 5){
+        if((commands[0] == DELETE) && (commands[1] == POST_SECOND_COMMAND) && (commands[2] == QUESTION_MARK) && (commands[3] == ID)){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+
 void run(vector<Student *> &students , vector<Professor *> &professors , UtAccount *ut_account_ptr){
     string command;
     while (true)
@@ -62,6 +88,11 @@ void run(vector<Student *> &students , vector<Professor *> &professors , UtAccou
                 }
                 post_command(command ,students ,professors , ut_account_ptr);
             }
+
+
+
+            cout << is_it_delete_post_command(command) << endl;
+
 
 
 
