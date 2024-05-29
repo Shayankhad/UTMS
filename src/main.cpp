b93382423f09bd31944b3e40b2b542701a86acaf
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-29 13:11:04 +0330

    solved problem of double_qouattaion in post command

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include "global.hpp"
-
+void check_for_double_quatation(string command){
+    stringstream ss;
+    ss << command;
+    string command_1;
+    getline(ss , command_1 , ' ');
+    string command_2;
+    getline(ss , command_2 , ' ');
+    string question_mark;
+    getline(ss , question_mark , ' ');
+    string arg_1;
+    getline(ss , arg_1 , ' ');
+    string arg_1_value;
+    getline(ss , arg_1_value , '"');
+    getline(ss , arg_1_value , '"');
+    string arg_2;
+    getline(ss , arg_2 , ' ');
+    getline(ss , arg_2 , ' ');
+    string test;
+    if(getline(ss , test , '"')){
+        if(!(test == "")){
+            throw BadRequest();
+        }
+    }
+    string arg_2_value;
+    int number_of_double_qoute = 0;
+    while(getline(ss , arg_2_value , ' ')){
+        for(char ch : arg_2_value){
+            if(ch == '"'){
+                number_of_double_qoute ++;
+            }
+        }
+    }
+    if(number_of_double_qoute == 0){
+        throw BadRequest();
+    }
+}
 
 bool is_it_post_command(string command){
     // POST post ? title “Good Day” message “Ye rooz joonane dige”
-
+    check_for_double_quatation(command);
     stringstream ss;
     ss << command;
     string command_1;
