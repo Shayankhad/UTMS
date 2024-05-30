950f334248e3bd95b5efb0d386883085ade1407d
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-30 13:28:26 +0330

    handeled some is_it_course_offer_command

--- src/main.cpp
+++ src/main.cpp
@@ -82,27 +82,19 @@ POST course_offer ? professor_id 810420432 course_id 1 capacity 70 time Sunday:1
             iteration ++;
         }
     }
-
     if(iteration != 15){
         return false;
     }
-
     for(vector<vector<__cxx11::basic_string<char>>>::size_type i = 3 ; i < 15 ; i = i +2 ){
         arg_commands.push_back({ commands[i] , commands[i + 1]});
     }
-
     vector<int> arg_order;
-    set_arg_order(arg_order , arg_commands);
-    for(int i = 0 ; i < arg_order.size() ; i++){
-        cout << arg_order[i] << endl;
+    if(!set_arg_order(arg_order , arg_commands)){
+        return false;
     }
     if(arg_order.size() != 6){
         return false;
     }
-
-    // check_number_type
-
-
     return true;
 }
 
