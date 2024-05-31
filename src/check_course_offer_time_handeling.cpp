c80398aea7343b73d802c47305d99b62af938fa4
Shayankhad <168594183+Shayankhad@users.noreply.github.com>
2024-05-31 15:45:10 +0330

    added presented course id

--- src/check_course_offer_time_handeling.cpp
+++ src/check_course_offer_time_handeling.cpp
@@ -5,6 +5,7 @@ void check_course_offer_time_handeling(vector<PresentedCourse *> presented_cours
     for(auto & presented_course : presented_course){
         if(!presented_course->check_start_and_finish_time_class_interference(hour , day , proffesor_id)){
             throw PermissionDenied();
+            
         }
     }
 }
